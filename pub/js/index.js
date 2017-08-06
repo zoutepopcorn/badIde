8c287949ee54674448749a1c0de9ee95179ad461
popcorn <popcorn@corn.corn>
2017-08-07 00:18:03 +0200

    ..

--- pub/js/index.js
+++ pub/js/index.js
@@ -16,7 +16,8 @@
  socket.on('data', (dat) => {
 			console.log(dat.data)
 			let o = $("#out");
-			o.append(`<cool>${dat.data}</cool>`);
+			let style = "test";
+			o.append(`<cool class=${style}>${dat.data}</cool>`);
 			o.scrollTop(o.prop("scrollHeight"));
  } );
 
@@ -29,8 +30,35 @@
 			// $.notify(data, {});
  });
 
+/*
  $('form').submit(function(e){
 		 e.preventDefault();
 		 var message = $('#chat_input').val();
 		 socket.emit('msg', {data: codeMirror.getValue()});
  });
+*/
+
+ $('#upload').click(() => {
+	 var message = $('#chat_input').val();
+	 socket.emit('msg', {data: codeMirror.getValue()});
+ });
+
+ $('#help').click(() => {
+	 socket.emit('msg', { data: "help()" });
+ });
+
+var loading = function(e) {
+  e.preventDefault();
+  e.stopPropagation();
+  e.target.classList.add('loading');
+  e.target.setAttribute('disabled','disabled');
+  setTimeout(function(){
+    e.target.classList.remove('loading');
+    e.target.removeAttribute('disabled');
+  },1500);
+};
+
+var btns = document.querySelectorAll('button');
+for (var i=btns.length-1;i>=0;i--) {
+  btns[i].addEventListener('click',loading);
+}
